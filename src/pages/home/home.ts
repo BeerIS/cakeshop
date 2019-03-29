6c2fcd4b9b68cf2f474ef242d42e01b78e210206
Phakpoom Ittirattanakomon <beerbc16@gmail.com>
2019-03-29 16:21:55 +0700

    edit page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  products:any=0;
+  constructor(public navCtrl: NavController, public navParam: NavParams, public http: Http) {
+    this.http.get('http://localhost:8080/products')
+      .map(res=>res.json())
+      .subscribe(data=>{
+        this.products = data;
+      });
 
   }
 
